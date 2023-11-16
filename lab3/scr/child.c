14c175df6aa347cd9cba5dc7e9307dbbd749131b
MDZhadnov <MDZhadnov@mai.education>
2023-11-16 22:53:42 +0300

    Finish first try (and last, i hope)

--- lab3/scr/child.c
+++ lab3/scr/child.c
@@ -1,14 +1,74 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include "unistd.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdbool.h>
 
+#define MEMORY_NAME "/1"
 
 
 int main(){ 
+    //int fd_virt = shm_open(MEMORY_NAME, O_EXCL | O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+    int fd_virt = shm_open(
+        MEMORY_NAME, 
+        O_CREAT | O_RDWR, 
+        S_IRUSR | S_IWUSR);
+    if(fd_virt == -1){
+        perror("\n(child)shm_open: here is a problem\n");
+        if(shm_unlink(MEMORY_NAME) == -1){
+            perror("(child)munmap: Here is a problem");
+            _exit(EXIT_FAILURE);
+        }
+        close(fd_virt);
+        _exit(EXIT_FAILURE);
+    }
+    struct stat statbuf;
+    if (fstat(STDIN_FILENO, &statbuf) < 0 ){
+        perror("\n(child)Can't get file size\n");
+        if(shm_unlink(MEMORY_NAME) == -1){
+            perror("(child)munmap: Here is a problem");
+            _exit(EXIT_FAILURE);
+        }
+        close(fd_virt);
+        _exit(EXIT_FAILURE);
+    }
+    if (ftruncate(fd_virt, statbuf.st_size) == -1) {
+        perror("\n(child)ftruncate: here is a problem\n");
+        if(shm_unlink(MEMORY_NAME) == -1){
+            perror("(child)munmap: Here is a problem");
+            _exit(EXIT_FAILURE);
+        }
+        close(fd_virt);
+        _exit(EXIT_FAILURE);
+    }
+    int* virtual = (int*)mmap(
+        NULL, 
+        statbuf.st_size, 
+        PROT_WRITE | PROT_READ, 
+        MAP_SHARED, 
+        fd_virt, 
+        0);
+    if(virtual == MAP_FAILED){
+        perror("\n(child)mmap: here is a problem\n");
+        if(shm_unlink(MEMORY_NAME) == -1){
+            perror("(child)munmap: Here is a problem");
+            _exit(EXIT_FAILURE);
+        }
+        close(fd_virt);
+        _exit(EXIT_FAILURE);
+    }
+
     int c = '\0';
     int tmp = 0, res = 0;
     int end_of_str = 0;
     char* to_res = "\0";
+
+    int count = 0;
 
     do{
         //c = getchar(); 
@@ -30,7 +90,8 @@ int main(){
                     //fprintf(stderr, "Division by zero");
                     //printf("%d\n", -1);
                     res = -1;
-                    write(STDOUT_FILENO, &res, sizeof(int));
+                    // write(STDOUT_FILENO, &res, sizeof(int));
+                    virtual[count] = res;
                     _exit(EXIT_FAILURE);
                 }
                 tmp = 0;
@@ -39,7 +100,9 @@ int main(){
         if(c == '\n' || c == EOF){
             // printf("%d\n", res);
             
-            write(STDOUT_FILENO, &res, sizeof(int));
+            // write(STDOUT_FILENO, &res, sizeof(int));
+            virtual[count] = res;
+            count++;
             end_of_str = 0;
             res = 0;
         }
@@ -48,7 +111,8 @@ int main(){
 
     res = -2; // end of work
     
-    write(STDOUT_FILENO, &res, sizeof(int));    
+    // write(STDOUT_FILENO, &res, sizeof(int)); 
+    virtual[count] = res;   
 
     return 0;
 }
--- lab3/scr/parent.c
+++ lab3/scr/parent.c
@@ -9,6 +9,8 @@
 #include <string.h>
 #include <stdbool.h>
 
+#define MEMORY_NAME "/1"
+
 int main(){
 
     char* filename = malloc(sizeof(char));
@@ -19,7 +21,8 @@ int main(){
         _exit(EXIT_FAILURE);
     }
 
-    int fd = open(filename, O_RDONLY);
+    int fd = 0;
+    fd = open(filename, O_RDONLY);
     if(fd == -1){
         perror("\nCan't open file\n");
         _exit(EXIT_FAILURE);
@@ -29,19 +32,7 @@ int main(){
         perror("\nCan't get file size\n");
         _exit(EXIT_FAILURE);
     }
-
-    int res_fd;
-    if ((res_fd = open("proc.txt", O_RDWR| O_CREAT, 0777)) == -1)
-    {
-        perror("\nError: can not open a file for mmapping.\n");
-        exit(EXIT_FAILURE);
-    }
-    int* virtual_file;
-    if((virtual_file = (int*)mmap(0, statbuf.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, res_fd, 0)) == MAP_FAILED){
-        perror("\nCan't make mapped file\n");
-        _exit(EXIT_FAILURE);
-    }
-
+    
     pid_t pid = fork();
     if (pid == -1) {
         perror("\nfork: Here is a problem\n");
@@ -49,7 +40,6 @@ int main(){
     }
     else if(pid == 0){ //child
         dup2(fd, STDIN_FILENO);
-        dup2(res_fd, STDOUT_FILENO);
         char* args[] = {"./child", NULL};
         if (execv(args[0], args) == -1){
             fprintf(stderr, "Unable to exec\n");
@@ -57,24 +47,53 @@ int main(){
         }
     }else{ //parent
         wait(0);
+        int fd_virt = shm_open(
+            MEMORY_NAME, 
+            O_RDONLY, 
+            S_IRUSR | S_IWUSR);
+        if(fd_virt == -1){
+            perror("\nshm_open: here is a problem\n");
+            _exit(EXIT_FAILURE);
+        }
+        int* virtual = (int*)mmap(
+            NULL, 
+            statbuf.st_size, 
+            PROT_READ, MAP_SHARED, 
+            fd_virt, 
+            0);
+        if(virtual == MAP_FAILED){
+            perror("\nmmap: here is a problem\n");
+            _exit(EXIT_FAILURE);
+        }
         int result = 0;
         for(int i = 0; i < statbuf.st_size; i++){
-            if(virtual_file[i] == -1){
+            if(virtual[i] == -1){
                 printf("Division by zero\n");
+                if(munmap(virtual, statbuf.st_size) == -1){
+                    perror("munmap: Here is a problem");
+                    _exit(EXIT_FAILURE);
+                }
+                if(shm_unlink(MEMORY_NAME) == -1){
+                    perror("munmap: Here is a problem");
+                    _exit(EXIT_FAILURE);
+                }
+                close(fd_virt);
                 _exit(EXIT_FAILURE);
             }
-            if(virtual_file[i] == -2){
+            if(virtual[i] == -2){
                 break;
             }
-            printf("%d\n", virtual_file[i]);
+            printf("%d\n", virtual[i]);
         }
+        if(munmap(virtual, statbuf.st_size) == -1){
+            perror("munmap: Here is a problem");
+            _exit(EXIT_FAILURE);
+        }
+        if(shm_unlink(MEMORY_NAME) == -1){
+            perror("munmap: Here is a problem");
+            _exit(EXIT_FAILURE);
+        }
+        close(fd_virt);
     }
-
-    if(munmap(virtual_file, statbuf.st_size) == -1){
-        perror("munmap: Here is a problem");
-        _exit(EXIT_FAILURE);
-    }
-    close(res_fd);
-    remove("proc.txt");
     return 0;
 }
